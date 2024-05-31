800cbf0288c814f14e8f1caf654bbcc707fbc5aa
harutoyyy <159074392+harutoyyy@users.noreply.github.com>
2024-05-31 12:55:25 -0700

    s

--- task2.py
+++ task2.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 
 
 window = tk.Tk()
-window.title("tk")
+window.title("t-town4")
 window.geometry("400x100")
 
 
@@ -14,8 +14,12 @@ dog = tk.Label(window, image=dogphoto,relief=SUNKEN)
 bord = tk.Entry(window,width=20)
 bord2 = tk.Entry(window,width=20)
 bord3 = tk.Entry(window,width=20)
-box = tk.Label(text="x")
-box2 = tk.Label(text="=" )
+box = tk.Label(window,text="s")
+box1 = tk.Label(window,text="Name" )
+box2 =tk.Label(window,text="Type" )
+box3 =tk.Label(window,text="Breed" )
+box4 =tk.Label(window,text="Birthday" )
+box5 =-tk.Label(window,text="=" )
 
 
 
