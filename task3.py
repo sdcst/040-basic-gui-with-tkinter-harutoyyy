8ab5b896d6a66f9c136e4711c3cdbb39b364ea2c
harutoyyy <159074392+harutoyyy@users.noreply.github.com>
2024-06-04 13:30:42 -0700

    s

--- task3.py
+++ task3.py
@@ -9,16 +9,19 @@ win.geometry("400x130")
 
 
 
+def change_color():
+   canvas.configure(bg='white')
 
+canvas= Canvas(win, bg='white')
 dogphoto = PhotoImage(file="dog.png")
 dog = tk.Label(win, image=dogphoto,relief=SUNKEN)
 text = tk.Label(win,text="Pochcco!")
 text2 = tk.Label(win,text="A cuddly little puppy!This is from the same creators who brought you Keropi and Kero Kero")
 
 
-dog.place(x=0,y=0)
-text.place(x=0,y=0)
-text2.place(x=0,y=0)
+dog.place(x=200,y=20)
+text.place(x=230,y=20)
+text2.place(x=200,y=100)
 
 
 
