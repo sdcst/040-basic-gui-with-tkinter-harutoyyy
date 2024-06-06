e95137817bcebf3a3e19c5e140dc05ffc7eb81c9
harutoyyy <haluomu0530@gmail.com>
2024-06-05 21:43:36 -0700

    save

--- task2.py
+++ task2.py
@@ -1,58 +1,50 @@
-import tkinter as tk 
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
 
 win = tk.Tk()
 win.title("t-town4")
-win.geometry("400x100")
-
+win.geometry("400x200")
 
 dogphoto = PhotoImage(file="dog.png")
-dog = tk.Label(win, image=dogphoto,relief=SUNKEN)
+dog = tk.Label(win, image=dogphoto, relief=SUNKEN)
 
-client = tk.Label(win,text="Client datebase")
+client = tk.Label(win, text="Client Database", font=("Helvetica", 16, "bold"))
+text = tk.Label(win, text="Name")
+text2 = tk.Label(win, text="Type")
+text3 = tk.Label(win, text="Breed")
+text4 = tk.Label(win, text="Owner")
+text5 = tk.Label(win, text="Birthdate")
 
-bord = tk.Entry(win,width=20)
+box1 = tk.Entry(win)
+box2 = tk.Entry(win)
+box3 = tk.Entry(win)
+box4 = tk.Entry(win)
+box5 = tk.Entry(win)
 
-text = tk.Label(win,text="Name")
-text2 = tk.Label(win,text="Type")
-text3 = tk.Label(win,text="Breed")
-text4 = tk.Label(win,text="Owner")
-text5 = tk.Label(win,text="Birthdate")
-
-previousbotton = tk.Button(win,text="previous")
-nextbotton = tk.Button(win,text="next")
-saveentry = tk.Button(win,text="Save Entry")
-
-box1 = tk.Entry(win,text="Name" )
-box2 =tk.Entry(win,text="Type" )
-box3 =tk.Entry(win,text="Breed" )
-box4 =tk.Entry(win,text="Owner" )
-box5 =tk.Entry(win,text="Birthdate" )
+previousbutton = tk.Button(win, text="Previous")
+nextbutton = tk.Button(win, text="Next")
+saveentry = tk.Button(win, text="Save Entry")
 
 
-dog.place(x=0,y=0)
-client.place(x=0,y=0)
-bord.place(x=0,y=0)
+dog.grid(row=0, column=0, rowspan=6, padx=5, pady=5)
+client.grid(row=0, column=1, columnspan=2, pady=5)
 
-text.place(x=0,y=0)
-text2.place(x=0,y=0)
-text3.place(x=0,y=0)
-text4.place(x=0,y=0) 
-text5.place(x=0,y=0)
+text.grid(row=1, column=1)
+text2.grid(row=2, column=1)
+text3.grid(row=3, column=1)
+text4.grid(row=4, column=1)
+text5.grid(row=5, column=1)
 
-previousbotton.place(x=0,y=0)
-nextbotton.place(x=0,y=0) 
-saveentry.place(x=0,y=0) 
+box1.grid(row=1, column=2)
+box2.grid(row=2, column=2)
+box3.grid(row=3, column=2)
+box4.grid(row=4, column=2)
+box5.grid(row=5, column=2)
 
-box1.place(x=0,y=0) 
-box2.place(x=0,y=0) 
-box3.place(x=0,y=0) 
-box4.place(x=0,y=0) 
-box5.place(x=0,y=0) 
-
-
-
+previousbutton.grid(row=6, column=0)
+nextbutton.grid(row=6, column=1)
+saveentry.grid(row=6, column=2)
 
 win.mainloop()
